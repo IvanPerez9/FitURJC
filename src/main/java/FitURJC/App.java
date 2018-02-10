1835472b3cefe32773516f81778b0570b07e1c81
dpemar <dpemar@hotmail.com>
2018-02-10 17:29:41 +0100

    Little Merge

--- src/main/java/FitURJC/App.java
+++ src/main/java/FitURJC/App.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class App {
 	
 	public static void main (String[] args) {
