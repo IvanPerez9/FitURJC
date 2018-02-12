c6b688a2950d6b02c6a00500b5b5f2ead73beb3d
IVÁN_P\Iván <ivan120695@gmail.com>
2018-02-12 12:10:51 +0100

    RegisterController

---
+++ src/main/java/FitURJC/ProfileSettings/RegisterController.java
@@ -0,0 +1,19 @@
+package FitURJC.ProfileSettings;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+import org.springframework.ui.Model;
+
+public class RegisterController {
+	
+	/*@RequestMapping("/Register/{id}")
+    public String userProfile(Model model, @PathVariable long id, HttpServletRequest request) {
+        
+    }*/
+	
+}
