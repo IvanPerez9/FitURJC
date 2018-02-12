f0faf1bece098c1ff4b7bfdde0b9b584bd29cbef
LAPTOP-FGGLNM4U\denis <deniseap6814@yahoo.com>
2018-02-12 11:20:57 +0100

    modified js code

--- FitURJC/js/administrationGraphics.js
+++ FitURJC/js/administrationGraphics.js
@@ -1,26 +1,43 @@
 $(document).ready(function(){
+    "use strict";
+
+    var datos= {
+        type: "pie",
+        data: {
+            datasets: [{
+                data: [
+                    40,
+                    22,
+                    38,
+                    17,
+                    62,
+                    10,
+                ],
+            backgroundColor: [
+                    "#F7464A",
+                    "#46BFBD",
+                    "#FDB45C",
+                    "#949FB1",
+                    "#4D5360",
+                    "#FDB450",
+                ],
+            }],
+            labels: [
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+            ]
+        },
+        options: {
+            responsive: true,
+        }
+    };
+
     //var popCanvas = $("#popChart");
     //var popCanvas = document.getElementById("popChart");
-    var popCanvas = document.getElementById("popChart").getContext("2d");
-
-    
-
-    window.pie barChart = new Chart(popCanvas, {
-    type: 'pie',
-    data: {
-        labels: ["Aerobic", "Body Combat", "Crossfit", "Step", "Spinning", "Zumba"],
-        datasets: [{
-        label: 'Average attendance to the collective classes in the week',
-        data: [128, 56, 235, 140, 68, 94],
-        backgroundColor: [
-            'rgba(17, 36, 119, 0.6)',
-            'rgba(39, 110, 192, 0.6)',
-            'rgba(51, 68, 87, 0.6)',
-            'rgba(27, 48, 167, 0.6)',
-            'rgba(131, 140, 189, 0.6)',
-            'rgba(88, 93, 119, 0.6)'
-        ]
-        }]
-    }
-    });
+    var popCanvas = document.getElementById('popChart').getContext('2d');
+    window.pie = new Chart(popCanvas,datos);
 });
