598d9635d0f30be808ff61d687a383793b714558
dpemar <dpemar@hotmail.com>
2018-02-17 20:11:31 +0100

    Facility in index test

---
+++ src/main/java/FitURJC/Facility/FacilityRepository.java
@@ -0,0 +1,10 @@
+package FitURJC.Facility;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacilityRepository extends JpaRepository <Facility, Long> {
+
+
+}
