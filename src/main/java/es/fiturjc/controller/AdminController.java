51a20b8d078523ca4d9f971027267d8a0fdd43cd
LAPTOP-FGGLNM4U\denis <deniseap6814@yahoo.com>
2018-02-24 23:29:49 +0100

    Modified Admin Controller

--- src/main/java/es/fiturjc/controller/AdminController.java
+++ src/main/java/es/fiturjc/controller/AdminController.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import es.fiturjc.model.Course;
+import es.fiturjc.model.User;
 import es.fiturjc.repository.UserRepository;
+import es.fiturjc.service.UserService;
 
 
 @Controller
@@ -19,33 +24,41 @@ public class AdminController {
 	@Autowired
 	private UserRepository usersRepository;
 	
-	@RequestMapping("13")
-	public String editProfile(Model model) {
-		return "das";
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/editProfile/{nickname}",method=RequestMethod.PUT)
+	public String editProfile(@PathVariable String nickname,@RequestBody MultiValueMap<String,String> params) {
+		userService.editUser(usersRepository.findByNickname(nickname), params);
+		return "admin";
 	}
 	
-	@RequestMapping("31")
-	public String registeredUsers() {
-		return "adsds";
+	@RequestMapping(value="/{nickname}/users", method=RequestMethod.GET)
+	public String registeredUsers(Model model, @PathVariable String nickname) throws Exception{
+		User user=usersRepository.findByNickname(nickname);
+		if(user.getRoles().contains("Admin")) {
+			List<User> users=usersRepository.findAll();
+			users.remove(user);
+			model.addAttribute("users",users);
+		}		
+		return "admin-controlUsers";
 	}
 	
-	@RequestMapping("2")
-	public String passwordChange() {
-		return "adasda";
+	@RequestMapping(value="{nickname}/changePass",method=RequestMethod.PUT)
+	public String passwordChange(@PathVariable String nickname,@RequestBody User user) {
+		userService.updateUserInfo(nickname, user);
+		return "admin-passwordChange";
 	}
-	@RequestMapping("/{id}")
-	public String registeredCourses(Model model,@PathVariable long id) throws Exception{
-		List<Course> courses=usersRepository.findOne(id).getCourses();		
-		model.addAttribute("courses", courses);		
+	
+	@RequestMapping(value="/{nickname}/courses", method=RequestMethod.GET)
+	public String registeredCourses(Model model,@PathVariable String nickname) throws Exception{
+		User user=usersRepository.findByNickname(nickname);
+		if(user.getRoles().contains("Admin")) {
+			List<Course> courses=user.getCourses();		
+			model.addAttribute("courses", courses);	
+		}
 		return "admin-controlActivities";
-	}//if you want to show all admin's courses, in which he is a coach and in which he is a user
-	
-	/*@RequestMapping("/{id}")
-	public String registeredCourses(Model model,@PathVariable long id) throws Exception{
-		User coach=usersRepository.findByRolesAndId("Admin",id);
-		List<Course> courses=coach.getCourses();		
-		model.addAttribute("courses", courses);		
-		return "admin-controlActivities";
-	}//it only shows the courses in which he is admin*/
-	
+	}
+
 }
+
