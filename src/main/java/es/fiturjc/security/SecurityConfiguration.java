37dfce6d6f5ea3e841850cb9fc16205dfe6f75b6
dpemar <dpemar@hotmail.com>
2018-03-13 17:40:39 +0100

    add buttom facebook
    Add button facebook, user save in database, TO DO:but not redirect to userprofile

--- src/main/java/es/fiturjc/security/SecurityConfiguration.java
+++ src/main/java/es/fiturjc/security/SecurityConfiguration.java
@@ -1,12 +1,20 @@
 package es.fiturjc.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.social.config.annotation.SocialConfigurer;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInController;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +23,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public UserRepositoryAuthenticationProvider authenticationProvider;
-
+	
+//	//facebook
+// 	@Autowired
+//    private UserDetailsService userDetailsService;
+ 	
+ 	@Autowired
+    private ConnectionFactoryLocator connectionFactoryLocator;
+  
+    @Autowired
+    private UsersConnectionRepository usersConnectionRepository;
+  
+    @Autowired
+    private FacebookConnectionSignup facebookConnectionSignup;
+     
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -27,11 +48,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/logout").permitAll();
 		http.authorizeRequests().antMatchers("/register").permitAll();
 		http.authorizeRequests().antMatchers("/newUser").permitAll();
+		//facebook
+ 		http.authorizeRequests().antMatchers("/signin/**","/signup/**").permitAll();
+//		http.apply(getSpringSocialConfigurer());
 
 		// User Pages
 		http.authorizeRequests().antMatchers("/courses").hasAnyRole("USER");
 		
-		//Admnin pages
+		//Admin pages
 		http.authorizeRequests().antMatchers("/adminPage/**").hasAnyRole("ADMIN");
 
 		// Login form
@@ -54,8 +78,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) { // mirar ejercicio de seguridad, http.httpBasic() y http.logout y tal 
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{ // mirar ejercicio de seguridad, http.httpBasic() y http.logout y tal 
 		// Database authentication provider
 		auth.authenticationProvider(authenticationProvider);
+		
+		//facebook
+//		auth.userDetailsService(userDetailsService);
 	}
+	
+	//facebook
+ 	@Bean
+     public ProviderSignInController providerSignInController() {
+         ((InMemoryUsersConnectionRepository) usersConnectionRepository)
+           .setConnectionSignUp(facebookConnectionSignup);
+          
+         return new ProviderSignInController(
+           connectionFactoryLocator, 
+           usersConnectionRepository, 
+           new FacebookSignInAdapter());
+     }
+	 	
+	 	
+// 	private SocialConfigurer getSpringSocialConfigurer() {
+// 	    SpringSocialConfigurer config = new SpringSocialConfigurer();
+// 	    config.alwaysUsePostLoginUrl(true);
+// 	    config.postLoginUrl("/profile?login=true");
+// 
+// 	    return config;
+// 	}
 }
