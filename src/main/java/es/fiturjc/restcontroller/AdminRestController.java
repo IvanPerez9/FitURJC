26afb5b4f21725dbb4ece67223a966f63709748a
VictoriaMG <victoriamarcosg7@gmail.com>
2018-03-16 18:35:37 +0100

    works add course in api
    Added functionality to add course to the api

--- src/main/java/es/fiturjc/restcontroller/AdminRestController.java
+++ src/main/java/es/fiturjc/restcontroller/AdminRestController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,163 +31,170 @@ import es.fiturjc.service.UserService;
 @RestController
 @RequestMapping("/api/admin")
 public class AdminRestController {
-	
-	@Autowired
-	private AdminService adminService;
-	
-	@Autowired
-	private CourseService courseService;
-	
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private UserComponent userComponent;
-	
-	// ************* USERS *****************
-	
-	/**
-	 * Get users List
-	 * @return
-	 */
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public ResponseEntity<List<User>> getUsers() {
-		List<User> users = userService.getUsers();
-		if (users != null) {
-			return new ResponseEntity<>(users, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	/**
-	 * Get specific user
-	 * @param id
-	 * @return
-	 */
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<User> getUser(@PathVariable long id) {
-		User user = userService.getUserbyID(id);
-		if (user != null) {
-			return new ResponseEntity<>(user, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	/**
-	 * Delete an User using the id. Checked
-	 * @param id
-	 * @return
-	 */
 
-	@DeleteMapping (value="/user/delete/{id}")
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public ResponseEntity<?> deleteUser (@PathVariable long id) {
-		if(adminService.deleteUser(id)) {
-			return new ResponseEntity<>(HttpStatus.OK);
-		}
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	}
-	
-	/**
-	 * METHOD PATCH TO EDIT THE INFORMATION THAT IS PROVIDED ONLY 
-	 * @param id
-	 * @param user
-	 * @return edited user
-	 */
-	
-	@PatchMapping (value="/user/edit/{id}")
-	public ResponseEntity<?> editUser(@PathVariable long id, @RequestBody User user) {
-		User userLogged = userService.findOne(userComponent.getLoggedUser().getId());
-	
-		if (userLogged.isAdmin() == true) {	
-			User userUpdated = userService.getUserbyID(id);		
-			if(userUpdated!=null) {
-				userUpdated = userService.updateUserInfo(id, user);
-				return new ResponseEntity<>(userUpdated, HttpStatus.OK);
-			}else {
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-			}
-		}else {
-			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-		}
-	}
-	
-	
-	//****************** COURSES **************
-	
-	/**
-	 * MIRAR PORQUE NO VA
-	 * @return
-	 */
-	
-	@RequestMapping(value = "/courses", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<List<Course>> getCourses() {
-		List<Course> courses = courseService.getAllCourses();
-		if (courses != null) {
-			return new ResponseEntity<>(courses, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	/**
-	 * 
-	 * @param course
-	 * @return new course
-	 */
-	
-	@RequestMapping(value = "/course/add", method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.CREATED)
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public ResponseEntity<?> addCourse(@RequestBody Course course) {
-		User userLogged = userService.findOne(userComponent.getLoggedUser().getId());
-		if (userLogged != null) {
-			courseService.createNewCourse(course, null); 
-			return new ResponseEntity<>(course, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	/**
-	 * Delete user by id
-	 * @param id
-	 * @return
-	 */
-	
-	@DeleteMapping(value="/course/delete/{id}") 
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public ResponseEntity<?> deleteCourse(@PathVariable long id) { 
-		if(courseService.deleteCourse(id)) {
-			return new ResponseEntity<>(HttpStatus.OK);
-		}
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	}
-	
-	/**
-	 * Edit course by id 
-	 * @param id
-	 * @param course
-	 * @return course
-	 */
-	
-	@PutMapping (value="/course/edit/{id}")
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public ResponseEntity<?> editCourse(@PathVariable long id , @RequestBody Course course) { 
-		Course c = courseService.findCourse(id);
-		if(c != null) {
-			courseService.editCourse(course, id);
-			return new ResponseEntity<>(course,HttpStatus.OK);
-		}
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		
-	}
-	
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserComponent userComponent;
+
+    // ************* USERS *****************
+
+    /**
+     * Get users List
+     *
+     * @return
+     */
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> users = userService.getUsers();
+        if (users != null) {
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Get specific user
+     *
+     * @param id
+     * @return
+     */
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        User user = userService.getUserbyID(id);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Delete an User using the id. Checked
+     *
+     * @param id
+     * @return
+     */
+
+    @DeleteMapping(value = "/user/delete/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> deleteUser(@PathVariable long id) {
+        if (adminService.deleteUser(id)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * METHOD PATCH TO EDIT THE INFORMATION THAT IS PROVIDED ONLY
+     *
+     * @param id
+     * @param user
+     * @return edited user
+     */
+
+    @PatchMapping(value = "/user/edit/{id}")
+    public ResponseEntity<?> editUser(@PathVariable long id, @RequestBody User user) {
+        User userLogged = userService.findOne(userComponent.getLoggedUser().getId());
+
+        if (userLogged.isAdmin() == true) {
+            User userUpdated = userService.getUserbyID(id);
+            if (userUpdated != null) {
+                userUpdated = userService.updateUserInfo(id, user);
+                return new ResponseEntity<>(userUpdated, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+
+    //****************** COURSES **************
+
+    /**
+     * MIRAR PORQUE NO VA
+     *
+     * @return
+     */
+
+    @RequestMapping(value = "/courses", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Course>> getCourses() {
+        List<Course> courses = courseService.getAllCourses();
+        if (courses != null) {
+            return new ResponseEntity<>(courses, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * @param course
+     * @return
+     */
+    //working now
+    @JsonView(Course.Details.class)
+    @JsonFormat(shape = JsonFormat.Shape.ARRAY)
+    @RequestMapping(value = "/course/add", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public ResponseEntity<Course> addCourse(@RequestBody Course course) {
+        if (userComponent.isLoggedUser()) {
+            courseService.save(course);
+            return new ResponseEntity<>(course, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    /**
+     * Delete user by id
+     *
+     * @param id
+     * @return
+     */
+
+    @DeleteMapping(value = "/course/delete/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> deleteCourse(@PathVariable long id) {
+        if (courseService.deleteCourse(id)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Edit course by id
+     *
+     * @param id
+     * @param course
+     * @return course
+     */
+
+    @PutMapping(value = "/course/edit/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> editCourse(@PathVariable long id, @RequestBody Course course) {
+        Course c = courseService.findCourse(id);
+        if (c != null) {
+            courseService.editCourse(course, id);
+            return new ResponseEntity<>(course, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
 }
