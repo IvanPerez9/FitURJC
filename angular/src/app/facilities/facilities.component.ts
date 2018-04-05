c5e8bd352c732b3d646876dad342d1950e0d470e
VictoriaMG <victoriamarcosg7@gmail.com>
2018-04-06 00:25:54 +0200

    First steps adapting facilities to angular

--- angular/src/app/facilities/facilities.component.ts
+++ angular/src/app/facilities/facilities.component.ts
@@ -8,11 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class FacilitiesComponent implements OnInit {
 imagePaths: string[];
 
+
   constructor() {
-    this.imagePaths = ['/assets/img/facilities/facilities_1.jpg', '/assets/img/facilities/facilities_2.jpg', '/assets/img/facilities/facilities_3.jpg', '/assets/img/facilities/facilities_4.jpg', '/assets/img/facilities/facilities_5.jpg', '/assets/img/facilities/facilities_6.jpg', '/assets/img/facilities/facilities_7.jpg', '/assets/img/facilities/facilities_8.jpg', '/assets/img/facilities/facilities_9.jpg', '/assets/img/facilities/facilities_10.jpg', '/assets/img/facilities/facilities_11.jpg', '/assets/img/facilities/facilities_12.jpg', '/assets/img/facilities/facilities_13.jpg', '/assets/img/facilities/facilities_14.jpg', '/assets/img/facilities/facilities_15.jpg', '/assets/img/facilities/facilities_16.jpg', '/assets/img/facilities/facilities_17.jpg', '/assets/img/facilities/facilities_18.jpg',
-      , '/assets/img/facilities/facilities_19.jpg', '/assets/img/facilities/facilities_20.jpg', '/assets/img/facilities/facilities_21.jpg', '/assets/img/facilities/facilities_22.jpg', '/assets/img/facilities/facilities_23.jpg', '/assets/img/facilities/facilities_24.jpg', '/assets/img/facilities/facilities_25.jpg', '/assets/img/facilities/facilities_26.jpg',
-      , '/assets/img/facilities/facilities_27.jpg', '/assets/img/facilities/facilities_28.jpg', '/assets/img/facilities/facilities_29.jpg', '/assets/img/facilities/facilities_30.jpg', '/assets/img/facilities/facilities_31.jpg', '/assets/img/facilities/facilities_32.jpg', '/assets/img/facilities/facilities_33']
-  }
+    this.imagePaths = ['/assets/img/courses/Aerobic.jpg', '/assets/img/courses/Body Combat.jpg', '/assets/img/courses/Boxing.jpg',
+      '/assets/img/courses/Cardio.jpg', '/assets/img/courses/CrossFit.jpg', '/assets/img/courses/Dumbbells.jpg']  }
 
   ngOnInit() {
   }
