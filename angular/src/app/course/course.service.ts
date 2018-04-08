6732c86ebd7dc0f3571f51c2f4185578078dcb03
IVÁN_P\Iván <ivan120695@gmail.com>
2018-04-08 10:36:19 +0200

    Course Service methods

--- angular/src/app/course/course.service.ts
+++ angular/src/app/course/course.service.ts
@@ -19,4 +19,34 @@ export class CourseService {
         return this.http.get(this.url);
     }
 
+    getCourse(courseId: number | string) {
+        return this.http.get(this.url + courseId);
+    }
+
+    createCourse(course: Course) {
+        return this.http.post(this.url , course);
+    }
+
+    deleteCourse(courseId: number | string) {
+        return this.http.delete(this.url + courseId);
+    }
+
+    editCourse (courseId: number | string , course: Course) {
+        return this.http.put(this.url + courseId , course);
+    }
+
+    subscribeToCourse(courseName: string) {
+        return this.http.put(this.url + courseName + '/members', null); // añadir el userLoggedd ???
+    }
+
+    setCoursePhoto (courseId: number, formData: FormData ) {
+        return this.http.put(this.url + courseId , formData);
+    }
+
+    private errors (error: any) {
+        console.error(error);
+        return Observable.throw('Error (' + error.status + '): ' + error.text() );
+    }
+
+
 }
