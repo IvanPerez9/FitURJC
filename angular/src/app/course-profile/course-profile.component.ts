54f6b9ae0e415b1245b2f74dde8acc6dd660384d
dpemar <dpemar@hotmail.com>
2018-04-09 18:36:14 +0200

    Starting course profile component

---
+++ angular/src/app/course-profile/course-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-profile',
+  templateUrl: './course-profile.component.html',
+  styleUrls: ['./course-profile.component.css']
+})
+export class CourseProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
