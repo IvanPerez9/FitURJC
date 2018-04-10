221e2049314a6a65c96caa26bac39f12a16839e1
IVÁN_P\Iván <ivan120695@gmail.com>
2018-04-10 17:06:11 +0200

    Minor changes due to Carl

--- angular/src/app/user/user.component.ts
+++ angular/src/app/user/user.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import {UserService} from "./user.service";
-import {User} from "./user.model";
-=======
+import {User} from './user.model';
 import { UserService } from '../user/user.service';
 import { Router, ActivatedRoute } from '@angular/router';
->>>>>>> 22a212d22c6f4cebfdd0c52b25017e21ac845d6a
 
 @Component({
   selector: 'app-user',
