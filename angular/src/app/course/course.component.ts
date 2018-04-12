6cc94436b365463cc79f40946560e5e4a48a0ba0
dpemar <dpemar@hotmail.com>
2018-04-13 00:37:37 +0200

    fixing section courses to home

--- angular/src/app/course/course.component.ts
+++ angular/src/app/course/course.component.ts
@@ -11,21 +11,17 @@ import { CourseProfileService } from '../course-profile/course-profile.service';
 })
 
 export class CourseComponent implements OnInit {
-  
+
   courses: Course[];
-  imagePaths: string[];
 
   constructor(private router: Router, private courseProfileService: CourseProfileService) {
-
-    this.imagePaths = ['/assets/img/courses/Aerobic.jpg', '/assets/img/courses/Body Combat.jpg', '/assets/img/courses/Boxing.jpg',
-      '/assets/img/courses/Cardio.jpg', '/assets/img/courses/CrossFit.jpg', '/assets/img/courses/Dumbbells.jpg']
   }
-  
+
 
   ngOnInit() {
     this.courseProfileService.getCourses().subscribe(
       courses => {
-        this.courses = courses.content;
+        this.courses = courses;
       },
       error => {
         console.log('ERROR!!! COURSES');
