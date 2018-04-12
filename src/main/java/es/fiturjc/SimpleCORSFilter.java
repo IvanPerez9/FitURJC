144ad92658e536f2fdb22669109ea3be5b88ef50
IVÁN_P\Iván <ivan120695@gmail.com>
2018-04-12 17:48:33 +0200

    CORS with PATCH

--- src/main/java/es/fiturjc/SimpleCORSFilter.java
+++ src/main/java/es/fiturjc/SimpleCORSFilter.java
@@ -18,7 +18,7 @@ public class SimpleCORSFilter implements Filter {
 
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, authorization");
 		chain.doFilter(req, res);
