21b8a4dbb10c73df9b198341b62f46d39b1895cc
IVÁN_P\Iván <ivan120695@gmail.com>
2018-04-13 11:42:43 +0200

    Make sure logOut when needed

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientBasicAuth } from '../HttpClient/httpClient';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private http: HttpClientBasicAuth) { }
 
   ngOnInit() {
+    this.http.logOut();
+    // Make sure there are no users logged
   }
 
 }
