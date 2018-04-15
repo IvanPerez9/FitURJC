ffec45a36e07ed1247903c0ce74bdb3c5c4d16bc
VictoriaMG <victoriamarcosg7@gmail.com>
2018-04-15 21:49:06 +0200

    Continue with edit profile
    Pequeños cambios, actualiza usuario pero no parece guardarlo...

--- angular/src/app/editProfile/editProfile.component.ts
+++ angular/src/app/editProfile/editProfile.component.ts
@@ -4,6 +4,7 @@ import { LoginService } from '../login/login.service';
 import { User } from '../user/user.model';
 import { UserService } from '../user/user.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {matchOtherValidator} from "../register/passwordValidator";
 
 
 @Component({
@@ -14,12 +15,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class EditProfileComponent implements OnInit {
 
-  userLogged: User;
+
   public editMode:number; // 0 nada - 1 mail - 2 pass  - 3 registro histórico de metas - 4 imagen
   userUpdated: FormGroup;
+  userLogged: User;
+  error_updated: boolean;
 
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
     this.userLogged = this.userService.getLoggedUser();
     this.editMode = 0;
   }
@@ -31,9 +34,31 @@ export class EditProfileComponent implements OnInit {
       nickname: new FormControl('', Validators.required),
       email: new FormControl('', Validators.required),
       age: new FormControl ('', Validators.required),
-      passwordHash: new FormControl('', Validators.required ),
+      passwordHash: new FormControl('', [Validators.required, matchOtherValidator('passwordHash')]),
     });
     console.log("Init UserComponent");
+  }
+  onSubmit(form:FormGroup){
+    this.userService.editUser(this.userLogged.id,this.userLogged).subscribe(
+      response=> {
+        console.log("User edited");
+        //Poner alguna comunicacion de que se ha efectuado bien o no la actualizacion
+        this.router.navigate(['/user/profile']);
+
+      },
+      error => {
+        console.log(error);
+        console.log(error.code);
+        if (error.status === 0) {
+          this.error_updated = true;
+        } else if (error.status === 401) {
+          this.error_updated = true;
+        } else if (error.status === 403) {
+          this.error_updated = true;
+        } else if (error.status === 405) {
+          this.error_updated = true;
+        }
+      });
   }
 
 
