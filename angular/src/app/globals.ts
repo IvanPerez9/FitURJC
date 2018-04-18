d91059caf8a4d2bf6a0c36f6d9803eb6ea129cb5
CarlUndert <andaquenocarlos@gmail.com>
2018-04-18 12:42:23 +0200

    little fix of lavanda

--- angular/src/app/globals.ts
+++ angular/src/app/globals.ts
@@ -1,4 +1,4 @@
-export const BASEURL: string = 'https://localhost:8443/api/';
+export const BASEURL: string = 'https://fiturjc.lavandadelpatio.es/api/';
 export const BASEURL_IMAGE: string = 'https://fiturjc.lavandadelpatio.es';
 export const USER_BASEURL: string = BASEURL + 'user/';
 export const USER_REGISTER: string = BASEURL + 'register';
